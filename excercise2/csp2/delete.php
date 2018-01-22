b740e8e7e968ee36787e0ba8d06fd1f22b513b10
Kristoffer Sobere <kristoffer.sobere@gmail.com>
2018-01-22 16:40:56 +0800

    csp2 update

--- excercise2/csp2/delete.php
+++ excercise2/csp2/delete.php
@@ -1,17 +1,20 @@
 <?php 
+
+require 'db/connection.php';
 $index = $_GET['index'];
 
-
-
-$items = file_get_contents('assets/items.json');
+/*$items = file_get_contents('assets/items.json');
 $items = json_decode($items, true);
+*/
 
-
-unset($items[$index]);
+/*unset($items[$index]);
 
 $file = fopen('assets/items.json', 'w');
 fwrite($file, json_encode($items, JSON_PRETTY_PRINT));
-fclose($file);
+fclose($file);*/
+
+$sql = "DELETE FROM rooms WHERE id ='$index'";
+mysqli_query($conn,$sql) or die(mysqli_query($conn));
 
 
 header('location: menu.php')	
--- excercise2/csp2/render_modal_body_endpoint_delete.php
+++ excercise2/csp2/render_modal_body_endpoint_delete.php
@@ -1,16 +1,22 @@
 <?php  
 	$index = $_POST['index'];
-	$string = file_get_contents("assets/items.json");
-	$items = json_decode($string, true);
+/*	$string = file_get_contents("assets/items.json");
+	$items = json_decode($string, true);*/
+	require 'db/connection.php';
 
-	$img = $items[$index]['img'];
+	$sql = "SELECT * FROM rooms WHERE id = '$index'";
+	$result = mysqli_query($conn,$sql);
+	$row = mysqli_fetch_assoc($result);
+	extract($row);
+
+	/*$img = $items[$index]['img'];
 	$name = $items[$index]['name'];
 	$description = $items[$index]['description'];
-	$price = $items[$index]['price'];
+	$price = $items[$index]['price'];*/
 
 		echo '<div class="row">';
 
-		echo "<div class='col-xs-10 text-center item_display'><form action='delete.php?index=$index' method='post'><img src='$img'><br>";
+		echo "<div class='col-xs-10 text-center item_display'><form action='delete.php?index=$index' method='post'><img src='$image'><br>";
 		echo "Name: $name<br>";
 		echo "Price: &#8369; $price<br>";
 		echo "<input type='submit' class='btn btn-success' value='Yes'>";
