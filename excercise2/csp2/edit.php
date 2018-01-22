3cc9515b6a48e4c2d44fdf29dc75914d87c8b1f7
Kristoffer Sobere <kristoffer.sobere@gmail.com>
2018-01-22 16:56:26 +0800

    csp2 update

--- excercise2/csp2/edit.php
+++ excercise2/csp2/edit.php
@@ -1,16 +1,29 @@
 <?php 
 
 	$index = $_GET['index'];
-	$string = file_get_contents("assets/items.json");
-	$items = json_decode($string, true);
+	require 'db/connection.php';
+	/*$string = file_get_contents("assets/items.json");
+	$items = json_decode($string, true);*/
 
-	$items[$index]['name'] = htmlspecialchars($_POST['name']);
+	$name = mysqli_real_escape_string($conn,$_POST['name']);
+	$description = mysqli_real_escape_string($conn,$_POST['description']);
+	$price = mysqli_real_escape_string($conn,$_POST['price']);
+
+	/*$items[$index]['name'] = htmlspecialchars($_POST['name']);
 	$items[$index]['description'] = htmlspecialchars($_POST['description']);
-	$items[$index]['price'] = htmlspecialchars($_POST['price']);
+	$items[$index]['price'] = htmlspecialchars($_POST['price']);*/
 
-	$file = fopen('assets/items.json', 'w');
+	$sql = "UPDATE rooms SET
+			name ='$name',
+			description = '$description',
+			price = '$price'
+			WHERE id ='$index'";
+
+	mysqli_query($conn,$sql) or die(mysqli_error($conn));
+
+	/*$file = fopen('assets/items.json', 'w');
 	fwrite($file,json_encode($items, JSON_PRETTY_PRINT));
-	fclose($file);
+	fclose($file);*/
 
 	header('location: menu.php');
 
--- excercise2/csp2/render_modal_body_endpoint.php
+++ excercise2/csp2/render_modal_body_endpoint.php
@@ -1,16 +1,18 @@
 <?php  
 	$index = $_POST['index'];
-	$string = file_get_contents("assets/items.json");
-	$items = json_decode($string, true);
+	/*$string = file_get_contents("assets/items.json");
+	$items = json_decode($string, true);*/
+	require 'db/connection.php';
 
-	$img = $items[$index]['img'];
-	$name = $items[$index]['name'];
-	$description = $items[$index]['description'];
-	$price = $items[$index]['price'];
+	$sql = "SELECT * FROM rooms WHERE id = '$index'";
+	$result = mysqli_query($conn,$sql);
+	$row = mysqli_fetch_assoc($result);
+	extract($row);
+
 
 		echo '<div class="row">';
 
-		echo "<div class='col-xs-10 text-center item_display'><form action='edit.php?index=$index' method='post'><img src='$img'><br>";
+		echo "<div class='col-xs-10 text-center item_display'><form action='edit.php?index=$index' method='post'><img src='$image'><br>";
 		echo "Name: <input type= 'text' name='name' value='$name'><br>";
 		echo "Description: <textarea name='description'>$description</textarea><br>";
 		echo "Price: &#8369; <input type='number' name='price' min=0 value='$price'><br>";
